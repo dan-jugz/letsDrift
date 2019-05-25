0dff6831fdc3d4131801d9a24f79b8f0cab18965
daniel njuguna <njugunadaniel364@gmail.com>
2019-05-25 20:48:29 +0300

    update photos on the site

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var elements = document.getElementsByClassName("column");
+
+var i;
+
+function four() {
+    for (i = 0; i < elements.length; i++) {
+      elements[i].style.flex = "25%"; 
+    }
+  }
